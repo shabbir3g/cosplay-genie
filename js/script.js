afda41dc45f7ac11142d49ebe9603d26a6fd0546
shabbir3g <mostafizshabbir@gmail.com>
2020-07-03 00:53:46 +0600

    gallery item hover and modal added

--- js/script.js
+++ js/script.js
@@ -8,33 +8,8 @@
 	$(document).ready(function(){
 
 
-		window.onload= function(){
-		    $(".proloader").hide(1000);
-		}
 
 		
-		 $('.single-portfolio').magnificPopup({
-		 	type: 'image',
-		 	gallery: {
-			  enabled: true, // set to true to enable gallery
-
-			  preload: [0,2], // read about this option in next Lazy-loading section
-
-			  navigateByImgClick: true,
-
-			  arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
-
-			  tPrev: 'Previous (Left arrow key)', // title for left button
-			  tNext: 'Next (Right arrow key)', // title for right button
-			  tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
-			},
-
-
-		 });
-
-		$('.parallax-window').parallax({
-		    speed: 0.8,
-		  });
 		
 		
 		 var shrinkHeader = 200;
